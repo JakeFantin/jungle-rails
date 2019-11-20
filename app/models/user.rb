b1733d5febb448d719b4cd1e9374a4c946a348f9
Jake <jakefantin@gmail.com>
2019-11-20 02:37:39 +0000

    user authentication: added .strip.downcase to the email in the autheticating class method as per scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   
   def self.authenticate_with_credentials(email, password)
-    User.find_by(email: email).try(:authenticate, password)
+    User.find_by(email: email.strip.downcase).try(:authenticate, password)
   end
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 3 }, confirmation: true
   validates :password_confirmation, presence: true
-  valdiates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
