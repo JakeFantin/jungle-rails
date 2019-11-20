ba5306316bdb323ba0878cc16e901d22acf54306
Jake <jakefantin@gmail.com>
2019-11-20 02:04:40 +0000

    user model: added validations for the fields and created a class method for user authetication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  def self.authenticate_with_credentials(email, password)
+    User.find_by(email: email).try(:authenticate, password)
+  end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 3 }, confirmation: true
+  validates :password_confirmation, presence: true
+  valdiates :email, presence: true, uniqueness: { case_sensitive: false }
 end
